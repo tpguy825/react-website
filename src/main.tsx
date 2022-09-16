de739d7df641de419f7a4e09af41557d8949ce0b
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-09-16 21:15:02 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,13 +8,15 @@ import ReactDOM from 'react-dom/client'
 // Components
 import Hero from './Hero'
 import Creations from './Creations'
+import Footer from './Footer'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   	<React.StrictMode>
 		<div className="px-4 py-5 my-5 text-center">
-			<Hero />
+			<Hero/>
             <div className="dropdown-divider"></div>
-			<Creations />
+			<Creations/>
 		</div>
+                <Footer/>
   	</React.StrictMode>
 )
