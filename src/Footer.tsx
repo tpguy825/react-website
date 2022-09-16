83d50e7b6c1a0cc8d7821751a86d33b7e3a835a9
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-09-16 20:39:10 +0100

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,9 +1,9 @@
 function Footer() {
     return(
-        <div class="container"> 
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"> 
-                <span class="mb-4 mb-md-0 text-muted"> 
-                    Made with <i class="bi bi-heart-fill"></i> by <a href="//tpguy825.github.io">tpguy825</a> -  <a href="//twitter.com/tobypayneyt"><i class="text-muted bi bi-twitter"></i></a> <a href="//instagram.com/tobypayneyt"><i class="text-muted bi bi-instagram"></i></a> <a href="//youtube.com/verydankmemes"><i class="text-muted bi bi-youtube"></i></a> <a href="//github.com/tpguy825"><i class="text-muted bi bi-github"></i></a> - <a href="//github.com/tpguy825/sophiepayne.ga">Source Code</a> 
+        <div className="container"> 
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"> 
+                <span className="mb-4 mb-md-0 text-muted"> 
+                    Made with <i className="bi bi-heart-fill"></i> by <a href="//tpguy825.github.io">tpguy825</a> -  <a href="//twitter.com/tobypayneyt"><i className="text-muted bi bi-twitter"></i></a> <a href="//instagram.com/tobypayneyt"><i className="text-muted bi bi-instagram"></i></a> <a href="//youtube.com/verydankmemes"><i className="text-muted bi bi-youtube"></i></a> <a href="//github.com/tpguy825"><i className="text-muted bi bi-github"></i></a> - <a href="//github.com/tpguy825/website">Source Code</a> 
                 </span> 
             </footer> 
         </div>
