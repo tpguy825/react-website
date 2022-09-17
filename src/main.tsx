a7802415d347f5fd314019a07de70618ad325817
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-09-17 07:57:43 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 // Bootstrap stuff
-import './scss/styles.scss'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import * as bootstrap from 'bootstrap'
+
 // React stuff
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Other files
+import './scss/styles.scss'
 
 // Components
 import Hero from './Hero'
@@ -14,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   	<React.StrictMode>
 		<div className="px-4 py-5 my-5 text-center">
 			<Hero/>
-            <div className="dropdown-divider"></div>
+                        <div className="dropdown-divider"></div>
 			<Creations/>
 		</div>
                 <Footer/>
