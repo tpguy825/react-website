d5298e723e118c37ec5ec4df6456a33129cef70f
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-09-17 08:20:40 +0100

    Create Head.tsx

---
+++ src/Head.tsx
@@ -0,0 +1,10 @@
+/** This is full of things to go into the <head> element of the webpage */
+function Head() {
+    return (
+        <>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"/>
+        </>
+    );
+}
+
+export default Head
