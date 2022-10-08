f7f04e392091990c680a62a12b1f455db3b62a94
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-10-08 08:43:42 +0100

    Fix outdated link

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -9,7 +9,7 @@ function Footer(): JSX.Element {
         <div className="container"> 
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3"> 
                 <span className="mb-4 mb-md-0"> 
-                    Made with {Icons.Heart} by tpguy825 -  <a rel="noopener" target="_blank" href="https://twitter.com/tobypayneyt">{Icons.Twitter}</a> <a rel="noopener" target="_blank" href="https://youtube.com/verydankmemes">{Icons.Youtube}</a> <a rel="noopener" target="_blank" href="https://github.com/tpguy825">{Icons.Github}</a> - <a rel="noopener" target="_blank" href="https://github.com/tpguy825/website" className="footer-icon">{Icons.Code} Source Code</a>
+                    Made with {Icons.Heart} by tpguy825 -  <a rel="noopener" target="_blank" href="https://twitter.com/tobypayneyt">{Icons.Twitter}</a> <a rel="noopener" target="_blank" href="https://youtube.com/verydankmemes">{Icons.Youtube}</a> <a rel="noopener" target="_blank" href="https://github.com/tpguy825">{Icons.Github}</a> - <a rel="noopener" target="_blank" href="https://github.com/tpguy825/react-website" className="footer-icon">{Icons.Code} Source Code</a>
                 </span> 
             </footer> 
         </div>
