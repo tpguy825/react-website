2bd9d002855c07d055e7d7276e994aa549548400
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-10-10 17:29:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import htmlPurge from 'vite-plugin-purgecss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), htmlPurge()],
 	resolve: {
 		alias: {
 			'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
