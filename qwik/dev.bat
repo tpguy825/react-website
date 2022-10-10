6073c13d169812b66f78e1218d34c2c7bf9efb99
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2022-10-10 17:38:34 +0100

    remove qwik as it has it's own repo

--- qwik/dev.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Starts vite
-npm run dev
--- qwik/src/components/ErrorPage.tsx
+++
@@ -1,9 +0,0 @@
-import { component$ } from "@builder.io/qwik";
-
-export default component$(() => {
-	return(
-		<div className="errorpage">
-			<p>404 - Something broke! <a href="/">Go Home</a></p>
-		</div>
-	)
-})
