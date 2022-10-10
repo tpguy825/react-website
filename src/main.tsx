95b3fa148b3ede253b8421eba8ab537157924cba
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2022-10-10 18:50:52 +0100

    undo lazy it changed my pagespeed score from 100 to 99

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import * as bootstrap from 'bootstrap'
 import { Partytown } from '@builder.io/partytown/react';
 
 // React stuff
-import React, { lazy, Suspense, useState } from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -13,22 +13,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './scss/styles.scss'
 
 // Components
-const Head = lazy(() => import('./Head'))
-const Hero = lazy(() => import('./Hero'))
-const Creations = lazy(() => import('./Creations'))
-const Footer = lazy(() => import('./Footer'))
-const Error = lazy(() => import('./ErrorPage'))
+import Head from './Head'
+import Hero from './Hero'
+import Creations from './Creations'
+import Footer from './Footer'
+import Error from './ErrorPage'
 
 function Main() {
 	return(
-		<Suspense fallback={<p>Loading...</p>}>
-			<div className="main px-4 text-center">
-					<Hero/>
-					<div className="dropdown-divider"></div>
-					<Creations/>
-				<Footer/>
-			</div>
-		</Suspense>
+		<div className="main px-4 text-center">
+				<Hero/>
+				<div className="dropdown-divider"></div>
+				<Creations/>
+			<Footer/>
+		</div>
 	)
 }
 
@@ -39,6 +37,7 @@ const router = createBrowserRouter([
 		errorElement: <Error />,
 	}
 ]);
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
